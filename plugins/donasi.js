f8506e6bd1b26b69149fb233f1787f90fd2f90c7
Amar Maulana Yusup <71469115+amarmyusup@users.noreply.github.com>
2022-07-14 02:05:11 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,6 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [0851-6101-1856]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
