a6b8643c22f06dea05741311efcde8004e83a681
Amar Maulana Yusup <71469115+amarmyusup@users.noreply.github.com>
2022-07-15 01:28:17 +0700

    Update addprem.js

--- plugins/addprem.js
+++ plugins/addprem.js
@@ -33,7 +33,7 @@ let handler = async (m, { conn, text, usedPrefix }) => {
   hl[0] = no(hl[0]) + "@s.whatsapp.net"
   hl[1] = text.split('|')[1]
   
-  if (!text) return conn.reply(m.chat, `*❏ GET NUMBER*\n\n• ${usedPrefix}prem number|days\n*Example:* ${usedPrefix}prem 6289654360447|99\n\n• ${usedPrefix}prem @tag|days\n*Example:* ${usedPrefix}prem @6289654360447|99`, m)
+  if (!text) return conn.reply(m.chat, `*❏ GET NUMBER*\n\n• ${usedPrefix}prem number|days\n*Example:* ${usedPrefix}prem 6285161011856|99\n\n• ${usedPrefix}prem @tag|days\n*Example:* ${usedPrefix}prem @6285161011856|99`, m)
   if (typeof db.data.users[hl[0]] == 'undefined') throw 'Pengguna tidak ada didalam data base'
   var jumlahHari = 86400000 * hl[1]
   // var jumlahHari = 1000 * text
