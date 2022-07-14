efb93ffa77d659d5c743a0b02e90f56c20625717
Amar Maulana Yusup <71469115+amarmyusup@users.noreply.github.com>
2022-07-15 01:20:59 +0700

    Update banuser.js

--- plugins/banuser.js
+++ plugins/banuser.js
@@ -26,7 +26,7 @@ let handler = async (m, { conn, text, usedPrefix }) => {
   hl[0] = no(hl[0]) + "@s.whatsapp.net"
   hl[1] = text.split('|')[1]
   
-  if (!text) return conn.reply(m.chat, `*❏ GET NUMBER*\n\n• ${usedPrefix}ban number|days\n*Example:* ${usedPrefix}ban 6289654360447|99\n\n• ${usedPrefix}ban @tag|days\n*Example:* ${usedPrefix}ban @6289654360447|99`, m)
+  if (!text) return conn.reply(m.chat, `*❏ GET NUMBER*\n\n• ${usedPrefix}ban number|days\n*Example:* ${usedPrefix}ban 6285161011856|99\n\n• ${usedPrefix}ban @tag|days\n*Example:* ${usedPrefix}ban @6285161011856|99`, m)
   if (typeof db.data.users[hl[0]] == 'undefined') throw 'Pengguna tidak ada didalam data base'
   var jumlahHari = 86400000 * hl[1]
   // var jumlahHari = 1000 * text
