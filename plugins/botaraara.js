386bf3f740291038534e67fedb91e610b6ce1494
Amar Maulana Yusup <71469115+amarmyusup@users.noreply.github.com>
2022-07-14 20:17:36 +0700

    Update and rename araara.js to botaraara.js

--- plugins/botaraara.js
+++ plugins/botaraara.js
@@ -1,12 +1,12 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m) => {
-let araaaa = fs.readFileSync('./mp3/WhatsApp-Audio-2021-06-21-at-11.33.19.opus')
+let araaaa = fs.readFileSync('./mp3/botaraara.opus')
 conn.sendFile(m.chat, araaaa, '', '', m, true)
 //conn.sendMessage(m.chat, araaaa, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 }
 
-handler.customPrefix = /^(ara ara)$/i
+handler.customPrefix = /^(ara ara|ara|araara|ara ara dong|ara ara lagi)$/i
 handler.command = new RegExp
 
 handler.limit = true
