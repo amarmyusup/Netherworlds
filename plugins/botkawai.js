feb19d7fcb7974cb9cc43ae06296ac619dbdcc76
Amar Maulana Yusup <71469115+amarmyusup@users.noreply.github.com>
2022-07-14 20:13:27 +0700

    Update and rename ainekawai.js to botkawai.js

--- plugins/botkawai.js
+++ plugins/botkawai.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let handler = async (m) => {
-let helloaine = fs.readFileSync('./mp3/kawai.opus')
-conn.sendFile(m.chat, helloaine, '', '', m, true)
+let hellobot = fs.readFileSync('./mp3/kawai.opus')
+conn.sendFile(m.chat, hellobot, '', '', m, true)
 }
 
 handler.customPrefix = /^(kawai|kawaii)$/i
