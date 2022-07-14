49e34a4e7ba5ec5152c037243ec93d93b935e5b4
Amar Maulana Yusup <71469115+amarmyusup@users.noreply.github.com>
2022-07-15 02:54:08 +0700

    Update botassalamualaikum.js

--- plugins/botassalamualaikum.js
+++ plugins/botassalamualaikum.js
@@ -1,4 +1,4 @@
-/*let fs = require('fs')
+let fs = require('fs')
 let handler = async (m) => {
 let assalamualaikum = fs.readFileSync('./mp3/botassalamualaikum.opus')
 conn.sendFile(m.chat, assalamualaikum, '', '', m, true)
@@ -8,4 +8,4 @@ handler.customPrefix = /^(assalamualaikum|assallamualaikum|assalamu\'alaikum|as\
 handler.command = new RegExp
 handler.limit = true
 
-module.exports = handler*/
+module.exports = handler
