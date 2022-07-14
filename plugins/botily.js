c51d875b093f7303b800a1e2a4c0def5f5476ff3
Amar Maulana Yusup <71469115+amarmyusup@users.noreply.github.com>
2022-07-14 20:01:17 +0700

    Update and rename ainesound-iloveyou.js to botily.js

--- plugins/botily.js
+++ plugins/botily.js
@@ -1,11 +1,11 @@
 /*let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m) => {
-let helloaine = fs.readFileSync('./mp3/PTT-20211218-WA0243.opus') 
-conn.sendFile(m.chat, helloaine, '', '', m, true)
+let helloaine = fs.readFileSync('./mp3/botily.opus') 
+conn.sendFile(m.chat, hellobot, '', '', m, true)
 }
 
-handler.customPrefix = /^(i love you|i ❤️ u)$/i
+handler.customPrefix = /^(i love you|i ❤️ u|ily|bot ily|ily bot|love you|i love you bot|lopyu|lopyubot|i love u bot|love u|love u bot)$/i
 handler.command = new RegExp
 
 handler.limit = true
