34e536382a597e991e9ffec7f3b65b4c18bea5db
Amar Maulana Yusup <71469115+amarmyusup@users.noreply.github.com>
2022-07-14 20:51:17 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -23,7 +23,7 @@ if (!args[0]) throw `*Perintah ini untuk mengunduh video tiktok dengan link*\n\n
 if (!args[0].match(/tiktok/gi)) throw `*Link salah! Perintah ini untuk mengunduh video tiktok dengan link*\n\ncontoh:\n${usedPrefix + command} https://vm.tiktok.com/ZGJAmhSrp/`
 let p = await  hxz.ttdownloader(args[0])
 const { nowm, wm, audio } = p
-// made by aine
+// made by amy
  conn.sendFile(m.chat, nowm, 'tiktok.mp4', `*${global.wm}*`, m)
 }
 handler.help = ['tiktok'].map(v => v + ' <url>')
@@ -42,7 +42,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     const url = video.no_watermark || video.no_watermark_hd || video.with_watermark || video.no_watermark_raw
     if (!url) throw 'Can\'t download video!'
     m.reply('Sedang diproses...')
-    conn.sendFile(m.chat, url, 'tiktok.mp4', `*© Aine*
+    conn.sendFile(m.chat, url, 'tiktok.mp4', `*©AMY*
 `.trim(), m)
 }
 handler.help = ['tiktok <url>']
