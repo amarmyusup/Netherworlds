795e102b4c11d073b84a2b2d07d7a3c136079548
Amar Maulana Yusup <71469115+amarmyusup@users.noreply.github.com>
2022-07-15 01:05:33 +0700

    Update codeshortlink.js

--- plugins/codeshortlink.js
+++ plugins/codeshortlink.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, args, usedPrefix }) => {
     if (args[0] == '882910' || args[0] == '443321' || args[0] == '662522' || args[0] == '322929' || args[0] == '562622' || args[0] == '432282' || args[0] == '002819' || args[0] == '715611' || args[0] == '882910' || args[0] == '882010' || args[0] == '937100' || args[0] == '736390' || args[0] == '762837' || args[0] == '028393' || args[0] == '625529' || args[0] == '727638' || args[0] == '992719' || args[0] == '092739' || args[0] == '727269' || args[0] == '629461' || args[0] == '239210') {
 
    if (new Date - global.db.data.users[m.sender].lastcode > 86800000) {
-     if (new Date - global.db.data.users[m.sender].lastcode < 86800000) throw `Anda sudah mengambill code gift, Code gift anda sudah kadaluarsa..\nTunggu besok iya kak..`
+     if (new Date - global.db.data.users[m.sender].lastcode < 86800000) throw `Anda sudah mengambill code gift, Code gift anda sudah kadaluarsa..\nTunggu besok ya kak..`
     conn.reply(m.chat, `*SELAMAT!*\n\nKamu telah mendapatkan\n+25000 XP\n+25000 Money\n+25000 Nabung Money\n+25 Limit`, m)
     global.db.data.users[m.sender].exp += 25000
     global.db.data.users[m.sender].limit += 25
