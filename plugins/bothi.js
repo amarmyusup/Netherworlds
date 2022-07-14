4b256b4df2ae4ee72c047929d71a39e88c29a89f
Amar Maulana Yusup <71469115+amarmyusup@users.noreply.github.com>
2022-07-15 02:33:08 +0700

    Update bothi.js

--- plugins/bothi.js
+++ plugins/bothi.js
@@ -1,13 +1,13 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn }) => {
-let helloaine = fs.readFileSync('./mp3/bothi.opus') 
-conn.sendFile(m.chat, hello, '', '', m, true)
+let hellobot = fs.readFileSync('./mp3/bothi.opus') 
+conn.sendFile(m.chat, hellobot, '', '', m, true)
 //conn.sendMessage(m.chat, hellobot, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 // await conn.sendMessage(m.chat, { audio: { url: hellobot }, mimetype: 'audio/mp4'}, m)
 }
 
-handler.customPrefix = /^(hi|hii|hiii|hii|hiii bot|hy|halo|hallo|helo|hello|hy bot|halo|hallo bot|helo bot|hello|Hi Bot|Bot|halo bot|hello bot)$/i
+handler.customPrefix = /^(hi|hii|hiii|hi bot|hii bot|hiii bot|hy|halo|hallo|helo|hello|hy bot|halo bot|hallo bot|helo bot|hello bot)$/i
 handler.command = new RegExp
 
 handler.limit = true
